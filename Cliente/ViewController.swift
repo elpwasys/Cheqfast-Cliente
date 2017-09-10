c0ad63b3b3a62293b0f413a84c7d0394069f26d8
pascke <everton.pascke@gmail.com>
2017-09-10 11:22:29 -0300

    Initial Commit

---
+++ Cliente/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Cliente
+//
+//  Created by Everton Luiz Pascke on 10/09/17.
+//  Copyright © 2017 Wasys. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
